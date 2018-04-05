eafa7d42e74b27ebab2d73f4d906f1400c588a52
Prashant More <prashantsmore@gmail.com>
2018-04-04 22:58:39 -0400

    Update and rename create.blade.php to index.blade.php

--- resources/views/bill/index.blade.php
+++ resources/views/bill/index.blade.php
@@ -5,32 +5,32 @@
 @endsection
 
 @section('content')
-    <h1>Split The Bill</h1>
+    <h1>BILL SPLITTER</h1>
 
-    <form method='POST' action='/bill/calculate'>
-        {{ csrf_field() }}
+    <form method='GET' action='/bill/calculate'>
 
         <div class='details'>* Required fields</div>
 
         <label for='split'>* Split How Many Ways ?</label>
-        <input type='text' name='split' id='split' value='{{ old('split') }}'>
+        <input type='text' maxlength='4' name='split' id='split' value='{{ old('split') }}'>
         @include('modules.error-field', ['field' => 'split'])
 
         <label for='tab'>* How Much Was The Tab ?</label>
-        <input type='text' maxlength='4' name='tab' id='tab' value='{{ old('tab') }}'>
+        <input type='text' maxlength='7' name='tab' id='tab' value='{{ old('tab') }}'>
         @include('modules.error-field', ['field' => 'tab'])
 
 
         <label for='tip'>* How Was The Service ? </label>
         <select name='tip' id='tip'>
-            <option value='choose'>Choose One ...</option>
-            <option value='18'>Good -18%</option>
-            <option value='10'>OK - 10%</option>
-            <option value='5'>Bad - 5%</option>
+            <option value='choose'>Select Tip Percentage</option>
+            <option value='18' @if (old('tip') == '18') {{ 'selected' }} @endif>Good -18%</option>
+            <option value='10' @if (old('tip') == '10') {{ 'selected' }} @endif>OK - 10%</option>
+            <option value='5' @if (old('tip') == '5') {{ 'selected' }} @endif>Bad - 5%</option>
         </select>
+
         @include('modules.error-field', ['field' => 'tip'])
 
-        <label >Do You Want To Open The Cookie ? <input  type='checkbox' name='fortuneCookie' value='Yes'/></label>
+        <label>Do You Want To Open The Cookie ? <input type='checkbox' name='fortuneCookie' value='Yes'/></label>
 
         <label> You can provide the optional food review below </label>
         <label><input type='radio'
@@ -42,8 +42,6 @@
         <label><input type='radio'
                       name='food'
                       value='bad'> Not Up To Mark</label>
-
-
 
         <input type='submit' value='Calculate' class='btn btn-primary'>
     </form>
